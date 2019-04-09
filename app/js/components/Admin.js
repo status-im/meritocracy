58637423d17c6d64a46aec64c95cb40cba829016
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-04-09 14:23:36 -0400

    add form validators and html form to add contributors

--- app/js/components/Admin.js
+++ app/js/components/Admin.js
@@ -1,12 +1,50 @@
 /*global web3*/
 import React from 'react';
+import {Button, Form} from 'react-bootstrap';
+import ValidatedForm from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import {required, isAddress} from '../validators';
 
 class Admin extends React.Component {
+  state = {
+    contributorName: '',
+    contributorAddress: ''
+  };
+
+  onChange = (name, e) => {
+    this.setState({[name]: e.target.value});
+  };
+
+  addContributor = (e) => {
+    e.preventDefault();
+    console.log('Submit', this.state);
+  };
 
   render() {
+    const {contributorAddress, contributorName} = this.state;
+
 
     return (<div>
-      <h3>Admin Panel</h3>
+      <h2>Admin Panel</h2>
+      <h3>Add a contributor</h3>
+      <ValidatedForm onSubmit={(e) => this.addContributor(e)}>
+        <Form.Group controlId="formContributor">
+          <Form.Label>Contributor name</Form.Label>
+          <Input type="text" placeholder="Name" value={contributorName}
+                 onChange={(e) => this.onChange('contributorName', e)}
+                 className="form-control"
+                 validations={[required]}/>
+        </Form.Group>
+
+        <Form.Group controlId="formAddress">
+          <Form.Label>Contributor address</Form.Label>
+          <Input type="text" placeholder="0x" value={contributorAddress}
+                 onChange={(e) => this.onChange('contributorAddress', e)}
+                 className="form-control"
+                 validations={[required, isAddress]}/>
+        </Form.Group>
+        <Button variant="primary" onClick={(e) => this.addContributor(e)}>Add</Button>
+      </ValidatedForm>
     </div>);
   }
 }
--- app/js/components/Header.js
+++ app/js/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Navbar, Nav} from 'react-bootstrap';
 
 const Header = () => (<Navbar bg="light" expand="lg">
-    <Navbar.Brand href="/#/">Meritocracy</Navbar.Brand>
+    <Navbar.Brand href="/#/">Status Meritocracy</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
