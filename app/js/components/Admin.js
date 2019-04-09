9b61c3da455ccde422e270219e5c503ca7d5b98f
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-04-09 13:29:40 -0400

    add header and basic admin page

---
+++ app/js/components/Admin.js
@@ -0,0 +1,14 @@
+/*global web3*/
+import React from 'react';
+
+class Admin extends React.Component {
+
+  render() {
+
+    return (<div>
+      <h3>Admin Panel</h3>
+    </div>);
+  }
+}
+
+export default Admin;
--- app/js/index.js
+++ app/js/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
     <App/>,
   document.getElementById('app')
